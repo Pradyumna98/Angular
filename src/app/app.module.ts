050bf4982951bf955f2571ae482c69d44b089f79
Pradyumna98 <vknarayana98@gmail.com>
2020-07-29 14:07:38 +0530

    Reactive Forms 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,16 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { AppComponent } from './app.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -17,20 +26,14 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
 
 
 import { LeaderService } from './services/leader.service';
 import { DishService } from './services/dish.service';
 
-import { MatDialogModule } from '@angular/material/dialog';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { LoginComponent } from './login/login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -58,7 +61,10 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
-    FormsModule 
+    MatSelectModule,
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule
 
   ],
   providers: [
